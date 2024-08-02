f2a7a2386ed711ead6143131fe08aa3a74a52740
KIM_TAEHOON <waps.kim_ta@gaxi.co.jp>
2024-08-02 14:57:28 +0900

    ※修正

--- src/views/AccessPage.vue
+++ src/views/AccessPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>アクセス</h1>
-    <p>(株) DXPRO 本社</p>
+    <p>DXPRO SOLUTIONS 本社</p>
     <p>〒 114 - 0014 東京都北区田端 4 - 21 - 14 - 402</p>
     <p>info@dxpro-sol.com</p>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6476.93680533215!2d139.75243087484!3d35.73929032678414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188deba9dc10b9%3A0x959dc904c9c6444b!2z44CSMTE0LTAwMTQg5p2x5Lqs6YO95YyX5Yy655Sw56uv77yU5LiB55uu77yS77yR4oiS77yR77yUIOaIuOaenee3j-alreOCt-ODo-ODs-ODnOODvOODq-Wkp-WSjOmDtw!5e0!3m2!1sja!2sjp!4v1722505564939!5m2!1sja!2sjp" width="100%" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> </div>
